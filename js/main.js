f359efa4c246fc701d1bee6ce829335438179ea5
Tim Trueman <trueman@twitter.com>
2013-02-19 11:53:24 -0800

    gh-pages initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  $('.typeahead').typeahead({
+    name: 'repos'
+  , prefetch: 'data/repos.json'
+  , template: [
+      '<p class="repo-language">{{language}}</p>'
+    , '<p class="repo-name">{{name}}</p>'
+    , '<p class="repo-description">{{description}}</p>'
+    ].join('')
+  , engine: Hogan
+  });
+
+  var haunt = ghostwriter.haunt({
+    input: '.typeahead'
+  , interval: 500
+  , manuscript: [
+      ghostwriter.noop
+    , 'Boots'
+    , ghostwriter.backspace.repeat(3)
+    , 'wer'
+    , ghostwriter.down.repeat(2)
+    , ghostwriter.enter
+    , ghostwriter.selectAll
+    , 'FlIgHt'
+    , ghostwriter.selectAll
+    , '  type'
+    , ghostwriter.right
+    , ghostwriter.esc
+    , ghostwriter.selectAll
+    ]
+  });
+
+  $('.btn-demo').on('click', function(e) {
+    e.stopPropagation();
+
+    $(this).addClass('is-pressed');
+    haunt.start();
+  });
+
+  $(document).on('click keydown', function(e) {
+    !e.ghostwriter && haunt.pause();
+  });
+});
