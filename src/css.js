f42148c7ca85d5ddd7b9f17599dd68af146d1750
Jake Harding <jharding@twitter.com>
2013-07-08 23:45:49 -0700

    Break out css and html into their own files.

---
+++ src/css.js
@@ -0,0 +1,60 @@
+/*
+ * typeahead.js
+ * https://github.com/twitter/typeahead
+ * Copyright 2013 Twitter, Inc. and other contributors; Licensed MIT
+ */
+
+var css;
+
+css = {
+  wrapper: {
+    position: 'relative',
+    display: 'inline-block'
+  },
+  hint: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    borderColor: 'transparent',
+    boxShadow: 'none'
+  },
+  input: {
+    position: 'relative',
+    verticalAlign: 'top',
+    backgroundColor: 'transparent'
+  },
+  dropdown: {
+    position: 'absolute',
+    top: '100%',
+    left: '0',
+    zIndex: '100',
+    display: 'none'
+  },
+  suggestions: {
+    display: 'block'
+  },
+  suggestion: {
+    whiteSpace: 'nowrap',
+    cursor: 'pointer'
+  },
+  suggestionChild: {
+    whiteSpace: 'normal'
+  }
+};
+
+// ie specific styling
+if (utils.isMsie()) {
+   // ie6-8 (and 9?) doesn't fire hover and click events for elements with
+   // transparent backgrounds, for a workaround, use 1x1 transparent gif
+  utils.mixin(css.query, {
+    backgroundImage: 'url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)'
+  });
+}
+
+// ie7 and under specific styling
+if (utils.isMsie() && utils.isMsie() <= 7) {
+  utils.mixin(css.wrapper, { display: 'inline', zoom: '1' });
+  // if someone can tell me why this is necessary to align
+  // the hint with the query in ie7, i'll send you $5 - @JakeHarding
+  utils.mixin(css.query, { marginTop: '-1px' });
+}
