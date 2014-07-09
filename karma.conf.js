6dbc2befed9187a63ada2f3efb359b523b74a458
Jake Harding <jharding@twitter.com>
2014-07-08 22:27:09 -0700

    Use jQuery v1.7 for development.

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ preprocessors = {
 
 // list of files / patterns to load in the browser
 files = [
-  'bower_components/jquery/dist/jquery.js',
+  'bower_components/jquery/jquery.js',
   'src/common/utils.js',
   'src/bloodhound/version.js',
   'src/bloodhound/tokenizers.js',
