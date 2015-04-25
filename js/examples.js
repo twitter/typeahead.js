bf2793da84046b2cfdab891b4e81c937a48a36d9
Jake Harding <jharding@twitter.com>
2015-04-25 03:14:42 -0700

    Prepare for v0.11

--- js/examples.js
+++ js/examples.js
@@ -1,216 +1,220 @@
 $(document).ready(function() {
 
-// the basics
-// ----------
+  // the basics
+  // ----------
 
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
+  var substringMatcher = function(strs) {
+    return function findMatches(q, cb) {
+      var matches, substringRegex;
 
-    // an array that will be populated with substring matches
-    matches = [];
+      // an array that will be populated with substring matches
+      matches = [];
 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
+      // regex used to determine if a string contains the substring `q`
+      substrRegex = new RegExp(q, 'i');
 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
-        matches.push({ value: str });
-      }
-    });
+      // iterate through the pool of strings and for any string that
+      // contains the substring `q`, add it to the `matches` array
+      $.each(strs, function(i, str) {
+        if (substrRegex.test(str)) {
+          matches.push(str);
+        }
+      });
 
-    cb(matches);
+      cb(matches);
+    };
   };
-};
 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
+  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  ];
 
-$('#the-basics .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  displayKey: 'value',
-  source: substringMatcher(states)
-});
+  $('#the-basics .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'states',
+    source: substringMatcher(states)
+  });
 
-// bloodhound
-// ----------
+  // bloodhound
+  // ----------
 
-// constructs the suggestion engine
-var states = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // `states` is an array of state names defined in "The Basics"
-  local: $.map(states, function(state) { return { value: state }; })
-});
+  // constructs the suggestion engine
+  var states = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: states
+  });
 
-// kicks off the loading/processing of `local` and `prefetch`
-states.initialize();
+  $('#bloodhound .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'states',
+    source: states
+  });
 
-$('#bloodhound .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  displayKey: 'value',
-  // `ttAdapter` wraps the suggestion engine in an adapter that
-  // is compatible with the typeahead jQuery plugin
-  source: states.ttAdapter()
-});
+  // prefetch
+  // --------
 
-// prefetch
-// --------
-
-var countries = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  limit: 10,
-  prefetch: {
+  var countries = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
     // url points to a json file that contains an array of country names, see
     // https://github.com/twitter/typeahead.js/blob/gh-pages/data/countries.json
-    url: '../data/countries.json',
-    // the json file contains an array of strings, but the Bloodhound
-    // suggestion engine expects JavaScript objects so this converts all of
-    // those strings
-    filter: function(list) {
-      return $.map(list, function(country) { return { name: country }; });
+    prefetch: '../data/countries.json'
+  });
+
+  // passing in `null` for the `options` arguments will result in the default
+  // options being used
+  $('#prefetch .typeahead').typeahead(null, {
+    name: 'countries',
+    source: countries
+  });
+
+  // remote
+  // ------
+
+  var bestPictures = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '../data/films/post_1960.json',
+    remote: {
+      url: '../data/films/queries/%QUERY.json',
+      wildcard: '%QUERY'
+    }
+  });
+
+  $('#remote .typeahead').typeahead(null, {
+    name: 'best-pictures',
+    display: 'value',
+    source: bestPictures
+  });
+
+  // default suggestions
+  // -------------------
+
+  var nflTeams = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    identify: function(obj) { return obj.team; },
+    prefetch: '../data/nfl.json'
+  });
+
+  function nflTeamsWithDefaults(q, sync) {
+    if (q === '') {
+      sync(nflTeams.get('Detroit Lions', 'Green Bay Packers', 'Chicago Bears'));
+    }
+
+    else {
+      nflTeams.search(q, sync);
     }
   }
-});
 
-// kicks off the loading/processing of `local` and `prefetch`
-countries.initialize();
+  $('#default-suggestions .typeahead').typeahead({
+    minLength: 0,
+    highlight: true
+  },
+  {
+    name: 'nfl-teams',
+    display: 'team',
+    source: nflTeamsWithDefaults
+  });
 
-// passing in `null` for the `options` arguments will result in the default
-// options being used
-$('#prefetch .typeahead').typeahead(null, {
-  name: 'countries',
-  displayKey: 'name',
-  // `ttAdapter` wraps the suggestion engine in an adapter that
-  // is compatible with the typeahead jQuery plugin
-  source: countries.ttAdapter()
-});
+  // custom templates
+  // ----------------
 
-// remote
-// ------
+  $('#custom-templates .typeahead').typeahead(null, {
+    name: 'best-pictures',
+    display: 'value',
+    source: bestPictures,
+    templates: {
+      empty: [
+        '<div class="empty-message">',
+          'unable to find any Best Picture winners that match the current query',
+        '</div>'
+      ].join('\n'),
+      suggestion: Handlebars.compile('<div><strong>{{value}}</strong> – {{year}}</div>')
+    }
+  });
 
-var bestPictures = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../data/films/post_1960.json',
-  remote: '../data/films/queries/%QUERY.json'
-});
+  // multiple datasets
+  // -----------------
 
-bestPictures.initialize();
+  var nbaTeams = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '../data/nba.json'
+  });
 
-$('#remote .typeahead').typeahead(null, {
-  name: 'best-pictures',
-  displayKey: 'value',
-  source: bestPictures.ttAdapter()
-});
+  var nhlTeams = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '../data/nhl.json'
+  });
 
-// custom templates
-// ----------------
+  $('#multiple-datasets .typeahead').typeahead({
+    highlight: true
+  },
+  {
+    name: 'nba-teams',
+    display: 'team',
+    source: nbaTeams,
+    templates: {
+      header: '<h3 class="league-name">NBA Teams</h3>'
+    }
+  },
+  {
+    name: 'nhl-teams',
+    display: 'team',
+    source: nhlTeams,
+    templates: {
+      header: '<h3 class="league-name">NHL Teams</h3>'
+    }
+  });
 
-$('#custom-templates .typeahead').typeahead(null, {
-  name: 'best-pictures',
-  displayKey: 'value',
-  source: bestPictures.ttAdapter(),
-  templates: {
-    empty: [
-      '<div class="empty-message">',
-      'unable to find any Best Picture winners that match the current query',
-      '</div>'
-    ].join('\n'),
-    suggestion: Handlebars.compile('<p><strong>{{value}}</strong> – {{year}}</p>')
-  }
-});
+  // scrollable dropdown menu
+  // ------------------------
 
-// multiple datasets
-// -----------------
+  $('#scrollable-dropdown-menu .typeahead').typeahead(null, {
+    name: 'countries',
+    limit: 10,
+    source: countries
+  });
 
-var nbaTeams = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../data/nba.json'
-});
+  // rtl
+  // ---
 
-var nhlTeams = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('team'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../data/nhl.json'
-});
+  var arabicPhrases = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: [
+      "الإنجليزية",
+      "نعم",
+      "لا",
+      "مرحبا",
+      "أهلا"
+    ]
+  });
 
-nbaTeams.initialize();
-nhlTeams.initialize();
-
-$('#multiple-datasets .typeahead').typeahead({
-  highlight: true
-},
-{
-  name: 'nba-teams',
-  displayKey: 'team',
-  source: nbaTeams.ttAdapter(),
-  templates: {
-    header: '<h3 class="league-name">NBA Teams</h3>'
-  }
-},
-{
-  name: 'nhl-teams',
-  displayKey: 'team',
-  source: nhlTeams.ttAdapter(),
-  templates: {
-    header: '<h3 class="league-name">NHL Teams</h3>'
-  }
-});
-
-// scrollable dropdown menu
-// ------------------------
-
-$('#scrollable-dropdown-menu .typeahead').typeahead(null, {
-  name: 'countries',
-  displayKey: 'name',
-  source: countries.ttAdapter()
-});
-
-var arabicPhrases = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('word'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  local: [
-    { word: "الإنجليزية" },
-    { word: "نعم" },
-    { word: "لا" },
-    { word: "مرحبا" },
-    { word: "أهلا" }
-  ]
-});
-
-arabicPhrases.initialize();
-
-$('#rtl-support .typeahead').typeahead({
-  hint: false
-},
-{
-  name: 'arabic-phrases',
-  displayKey: 'word',
-  source: arabicPhrases.ttAdapter()
-});
+  $('#rtl-support .typeahead').typeahead({
+    hint: false
+  },
+  {
+    name: 'arabic-phrases',
+    source: arabicPhrases
+  });
 });
--- js/main.js
+++ js/main.js
@@ -1,54 +1,62 @@
 $(document).ready(function() {
-  var haunt, repos;
+  var engine, remoteHost, template, empty;
 
-  repos = new Bloodhound({
-    datumTokenizer: function(d) { return d.tokens; },
+  $.support.cors = true;
+
+  remoteHost = 'http://typeahead-js-twitter-api-proxy.herokuapp.com';
+  template = Handlebars.compile($("#result-template").html());
+  empty = Handlebars.compile($("#empty-template").html());
+
+  engine = new Bloodhound({
+    identify: function(o) { return o.id_str; },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: 'data/repos.json'
-  });
-
-  repos.initialize();
-
-  $('.typeahead').typeahead(null, {
-    name: 'repos',
-    source: repos.ttAdapter(),
-    templates: {
-      suggestion: Handlebars.compile([
-        '<p class="repo-language">{{language}}</p>',
-        '<p class="repo-name">{{name}}</p>',
-        '<p class="repo-description">{{description}}</p>'
-      ].join(''))
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name', 'screen_name'),
+    dupDetector: function(a, b) { return a.id_str === b.id_str; },
+    prefetch: remoteHost + '/demo/prefetch',
+    remote: {
+      url: remoteHost + '/demo/search?q=%QUERY',
+      wildcard: '%QUERY'
     }
   });
 
-  haunt = ghostwriter.haunt({
-    input: '#main-typeahead'
-  , interval: 500
-  , manuscript: [
-      ghostwriter.noop
-    , 'Boots'
-    , ghostwriter.backspace.repeat(3)
-    , 'wer'
-    , ghostwriter.down.repeat(2)
-    , ghostwriter.enter
-    , ghostwriter.selectAll
-    , 'FlIgHt'
-    , ghostwriter.selectAll
-    , 'type'
-    , ghostwriter.right
-    , ghostwriter.esc
-    , ghostwriter.selectAll
-    ]
+  // ensure default users are read on initialization
+  engine.get('1090217586', '58502284', '10273252', '24477185')
+
+  function engineWithDefaults(q, sync, async) {
+    if (q === '') {
+      sync(engine.get('1090217586', '58502284', '10273252', '24477185'));
+      async([]);
+    }
+
+    else {
+      engine.search(q, sync, async);
+    }
+  }
+
+  $('#demo-input').typeahead({
+    hint: $('.Typeahead-hint'),
+    menu: $('.Typeahead-menu'),
+    minLength: 0,
+    classNames: {
+      open: 'is-open',
+      empty: 'is-empty',
+      cursor: 'is-active',
+      suggestion: 'Typeahead-suggestion',
+      selectable: 'Typeahead-selectable'
+    }
+  }, {
+    source: engineWithDefaults,
+    displayKey: 'screen_name',
+    templates: {
+      suggestion: template,
+      empty: empty
+    }
+  })
+  .on('typeahead:asyncrequest', function() {
+    $('.Typeahead-spinner').show();
+  })
+  .on('typeahead:asynccancel typeahead:asyncreceive', function() {
+    $('.Typeahead-spinner').hide();
   });
 
-  $('.btn-demo').on('click', function(e) {
-    e.stopPropagation();
-
-    $(this).addClass('is-pressed');
-    haunt.start();
-  });
-
-  $(document).on('click keydown', function(e) {
-    !e.ghostwriter && haunt.pause();
-  });
 });
