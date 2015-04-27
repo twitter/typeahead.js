f885f4863615937acb74aeae1b38602cbecfda37
Jake Harding <jharding@twitter.com>
2015-04-26 18:03:07 -0700

    Make indexing remote a top-level option

--- src/bloodhound/bloodhound.js
+++ src/bloodhound/bloodhound.js
@@ -20,6 +20,7 @@ var Bloodhound = (function() {
     this.sorter = o.sorter;
     this.identify = o.identify;
     this.sufficient = o.sufficient;
+    this.indexRemote = o.indexRemote;
 
     this.local = o.local;
     this.remote = o.remote ? new Remote(o.remote) : null;
@@ -132,6 +133,9 @@ var Bloodhound = (function() {
     search: function search(query, sync, async) {
       var that = this, local;
 
+      sync = sync || _.noop;
+      async = async || _.noop;
+
       local = this.sorter(this.index.search(query));
 
       // return a copy to guarantee no changes within this scope
@@ -160,12 +164,9 @@ var Bloodhound = (function() {
         });
 
         // #1148: Should Bloodhound index remote datums?
-        if (that.remote.indexResponse) {
-          that.index.add(nonDuplicates);
-          nonDuplicates = that.index.search(query);
-        }
+        that.indexRemote && that.add(nonDuplicates);
 
-        async && async(nonDuplicates);
+        async(nonDuplicates);
       }
     },
 
