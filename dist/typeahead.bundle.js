5f98c6c6e2288e2958c202d447225596e0b23c21
Per Nilsson <per.nilsson@formidablelabs.com>
2015-05-04 17:24:53 -0700

    Add dist files

--- dist/typeahead.bundle.js
+++ dist/typeahead.bundle.js
@@ -1720,8 +1720,8 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        rendered += suggestions.length;
                         that._append(query, suggestions.slice(0, that.limit - rendered));
+                        rendered += suggestions.length;
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
