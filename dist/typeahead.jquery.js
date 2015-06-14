d39ef247aa181c1925d7a520d92ac44b522cd4ad
Tom Rees <zephod@gmail.com>
2015-06-14 22:42:27 +0100

    Bugfix in counting rendered async results

--- dist/typeahead.jquery.js
+++ dist/typeahead.jquery.js
@@ -807,8 +807,9 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
+                        suggestions = suggestions.slice(0, that.limit - rendered);
+                        that._append(query, suggestions);
                         rendered += suggestions.length;
-                        that._append(query, suggestions.slice(0, that.limit - rendered));
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
