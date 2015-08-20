be3f07b925dbbed159ab7d7af0cc8545c1ad8128
Jae Hong Lee <hongz1@users.noreply.github.com>
2015-08-19 22:08:23 -0400

    autoSelect Option
    ‘autoSelect’ option allows to select suggestion in the input box with
    hitting enter or tab key. This option is available to both single and
    multiple datasets.

--- src/typeahead/menu.js
+++ src/typeahead/menu.js
@@ -26,6 +26,7 @@ var Menu = (function() {
     // the latest query #update was called with
     this.query = null;
     this.datasets = _.map(o.datasets, initializeDataset);
+    this.autoSelect = !!o.autoSelect;
 
     function initializeDataset(oDataset) {
       var node = that.$node.find(oDataset.node).first();
@@ -121,6 +122,11 @@ var Menu = (function() {
     },
 
     open: function open() {
+      //autoSelect:true - the top suggestion will be selected if input box has been focused
+      var $suggestionList = $(this.$node[0].children[0]).find(this.selectors.suggestion + this.selectors.selectable);
+      if (this.autoSelect && $suggestionList.length > 0) {
+        $suggestionList.first().addClass(this.classes.cursor);
+      }
       this.$node.addClass(this.classes.open);
     },
 
