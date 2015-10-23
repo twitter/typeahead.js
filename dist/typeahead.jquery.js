95f45e80fcdf1d0bca0c37cc0b8f2060fbd68ec0
Angelko Krstanovic <angelko4fun@gmail.com>
2015-10-23 10:54:38 -0700

    Moving 'rendered += suggestions.length;' down by 1 line
    Bumping version number to 0.11.2

--- dist/typeahead.jquery.js
+++ dist/typeahead.jquery.js
@@ -1,5 +1,5 @@
 /*!
- * typeahead.js 0.11.1
+ * typeahead.js 0.11.2
  * https://github.com/twitter/typeahead.js
  * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
  */
@@ -807,8 +807,8 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        rendered += suggestions.length;
                         that._append(query, suggestions.slice(0, that.limit - rendered));
+                        rendered += suggestions.length;
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
