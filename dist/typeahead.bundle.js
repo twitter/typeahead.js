f70a8eac475bc80d14d0e4c5b5cdbdb0860c7ef6
Micah Blu <micahblu@gmail.com>
2016-01-20 22:27:45 -0800

    Adds revised dist files w/renamed typehead module

--- dist/typeahead.bundle.js
+++ dist/typeahead.bundle.js
@@ -1,7 +1,7 @@
 /*!
  * typeahead.js 0.11.1
  * https://github.com/twitter/typeahead.js
- * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
+ * Copyright 2013-2016 Twitter, Inc. and other contributors; Licensed MIT
  */
 
 (function(root, factory) {
@@ -919,7 +919,7 @@
 
 (function(root, factory) {
     if (typeof define === "function" && define.amd) {
-        define("typeahead.js", [ "jquery" ], function(a0) {
+        define("typeahead", [ "jquery" ], function(a0) {
             return factory(a0);
         });
     } else if (typeof exports === "object") {
