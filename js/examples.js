0db764a5740cc5ee4bb40981f6981a4a967696db
Thomas Clausen <kontakt@thomasclausen.dk>
2016-05-26 13:54:58 +0200

    Declaration of wrong variable
    The variable `substringRegex` but instead it should be `substrRegex`.
    Minor typo, but outputs error if using `'use strict'`.

--- js/examples.js
+++ js/examples.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
   var substringMatcher = function(strs) {
     return function findMatches(q, cb) {
-      var matches, substringRegex;
+      var matches, substrRegex;
 
       // an array that will be populated with substring matches
       matches = [];
