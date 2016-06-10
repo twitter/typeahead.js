9c1d3969688efc94242df118630778975d029a71
Danil Pavlov <dp@friendwork.ru>
2016-06-10 16:12:25 +0300

    Change getting arrays intersection

--- src/bloodhound/search_index.js
+++ src/bloodhound/search_index.js
@@ -163,29 +163,8 @@ var SearchIndex = window.SearchIndex = (function() {
   }
 
   function getIntersection(arrayA, arrayB) {
-    var ai = 0, bi = 0, intersection = [];
-
-    arrayA = arrayA.sort();
-    arrayB = arrayB.sort();
-
-    var lenArrayA = arrayA.length, lenArrayB = arrayB.length;
-
-    while (ai < lenArrayA && bi < lenArrayB) {
-      if (arrayA[ai] < arrayB[bi]) {
-        ai++;
-      }
-
-      else if (arrayA[ai] > arrayB[bi]) {
-        bi++;
-      }
-
-      else {
-        intersection.push(arrayA[ai]);
-        ai++;
-        bi++;
-      }
-    }
-
-    return intersection;
+    return arrayA.filter(function (n) {
+                return arrayB.indexOf(n) !== -1;
+            });
   }
 })();
