bd0092926abe42962e7ef833313fdd3d43609bd1
Richard Francis <rich0886@gmail.com>
2016-09-15 16:18:15 +0100

    0.12.0

--- dist/typeahead.jquery.js
+++ dist/typeahead.jquery.js
@@ -1,7 +1,7 @@
 /*!
  * typeahead.js 0.11.1
  * https://github.com/twitter/typeahead.js
- * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
+ * Copyright 2013-2016 Twitter, Inc. and other contributors; Licensed MIT
  */
 
 (function(root, factory) {
