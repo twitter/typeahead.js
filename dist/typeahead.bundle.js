c6e8948959fe6d2963a782f5e7419a5412f4dbcb
jony89 <ariel@emath.co.il>
2018-02-16 01:46:47 +0200

    fix: dist

--- dist/typeahead.bundle.js
+++ dist/typeahead.bundle.js
@@ -1,7 +1,7 @@
 /*!
- * typeahead.js 0.11.1
+ * typeahead.js 2.0.0
  * https://github.com/twitter/typeahead.js
- * Copyright 2013-2015 Twitter, Inc. and other contributors; Licensed MIT
+ * Copyright 2013-2018 Twitter, Inc. and other contributors; Licensed MIT
  */
 
 (function(root, factory) {
@@ -9,10 +9,10 @@
         define("bloodhound", [ "jquery" ], function(a0) {
             return root["Bloodhound"] = factory(a0);
         });
-    } else if (typeof exports === "object") {
+    } else if (typeof module === "object" && module.exports) {
         module.exports = factory(require("jquery"));
     } else {
-        root["Bloodhound"] = factory(jQuery);
+        root["Bloodhound"] = factory(root["jQuery"]);
     }
 })(this, function($) {
     var _ = function() {
@@ -151,7 +151,7 @@
             noop: function() {}
         };
     }();
-    var VERSION = "0.11.1";
+    var VERSION = "2.0.0";
     var tokenizers = function() {
         "use strict";
         return {
@@ -525,8 +525,12 @@
         }
         function getIntersection(arrayA, arrayB) {
             var ai = 0, bi = 0, intersection = [];
-            arrayA = arrayA.sort();
-            arrayB = arrayB.sort();
+            arrayA = arrayA.sort(function(a, b) {
+                return a - b;
+            });
+            arrayB = arrayB.sort(function(a, b) {
+                return a - b;
+            });
             var lenArrayA = arrayA.length, lenArrayB = arrayB.length;
             while (ai < lenArrayA && bi < lenArrayB) {
                 if (arrayA[ai] < arrayB[bi]) {
@@ -922,10 +926,10 @@
         define("typeahead.js", [ "jquery" ], function(a0) {
             return factory(a0);
         });
-    } else if (typeof exports === "object") {
+    } else if (typeof module === "object" && module.exports) {
         module.exports = factory(require("jquery"));
     } else {
-        factory(jQuery);
+        factory(root["jQuery"]);
     }
 })(this, function($) {
     var _ = function() {
